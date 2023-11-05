1df4c3bd4b46c75247a86eab9bafc3285c9470bc
Viviane-Maria <viviane.m.oliveira.s@gmail.com>
2023-11-05 02:03:48 -0300

    project create

---
+++ src/app/components/footer/links-playstation/links-playstation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-links-playstation',
+  templateUrl: './links-playstation.component.html',
+  styleUrls: ['./links-playstation.component.css']
+})
+export class LinksPlaystationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
